b6fb56546ffce8e31415fff4f5a170a475364425
onkar2612 <90840627+onkar2612@users.noreply.github.com>
2021-10-26 18:39:34 -0700

    Delete move.py

--- Programs/move.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-tar='C:\\Users\\onkar\\Desktop\\java\\Programs'
-des='C:\\Users\\onkar\\Desktop\\java\\Recurrsion_programs'
-for i in os.listdir(tar):
-    if('Recurrsion' in i):
-        shutil.move(i,des)
